977ea491d57a1f9c12a1628d3351b6b70f90b1b8
Gumani Dakalo Luvhengo <dakalo.luvhengo17@gmail.com>
2024-01-07 20:37:09 +0200

    finished the core requirements

--- src/main/java/com/number/game/MainController.java
+++ src/main/java/com/number/game/MainController.java
@@ -1,5 +1,6 @@
 package com.number.game;
 
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -15,31 +16,27 @@ import java.util.ResourceBundle;
 public class MainController implements Initializable {
 
     @FXML
+    protected Label lblGuessesRemaining;
+    @FXML
     private TextField txtRandomGenerated;
     @FXML
     private TextField txtRandomGuess;
     private Random random;
     private int guessedNumber;
+    private int guessesRemaining;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         random = new Random();
+        guessesRemaining = 10;
+        lblGuessesRemaining.setText(String.valueOf(guessesRemaining));
+
         txtRandomGuess.addEventFilter(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent keyEvent) {
-                int input =0;
-                try {
-                    input = Integer.parseInt(keyEvent.getCharacter());
-                }catch (NumberFormatException ignore){
+
+                if (!keyEvent.getCharacter().matches("[0-9]")) {
+                    //erase the textfield if a number is not entered
                     keyEvent.consume();
-                    return;
-                }
-
-                String character = keyEvent.getCharacter();
-
-                if(character.matches("[0-9]")){
-                    if(keyEvent.getSource() instanceof TextField textField && !textField.getText().isEmpty()){
-                    }
-
                 }
             }
         });
@@ -49,17 +46,35 @@ public class MainController implements Initializable {
     }
 
     @FXML
-    protected void onClickStart(ActionEvent actionEvent) {
-
-
-        try {
-            guessedNumber = Integer.parseInt(txtRandomGuess.getText());
-        }catch (NumberFormatException ignored){
-            //wrong format
+    protected void onGuessClick(ActionEvent actionEvent) {
+        if(txtRandomGenerated.getText().isEmpty()){
+            CustomDialog.show("Generate number","You must 1st generate a random number");
+            return;
+        }
+        if(txtRandomGuess.getText().isEmpty()){
+            CustomDialog.show("Input number","You must enter a number");
+            return;
         }
 
+        if(guessesRemaining == 0){
+            CustomDialog.show("Random guess","Your have finished your number of attempts.");
+            return;
+        }
+        int guessedNumber = Integer.parseInt(txtRandomGuess.getText());
 
-
+        int generated = Integer.parseInt(txtRandomGenerated.getText());
+        if(guessedNumber == generated){
+            //correct
+            CustomDialog.show("Random guess","You guessed correctly.");
+        }else if(guessedNumber > generated){
+            //too high
+            CustomDialog.show("Random guess","You guessed too high.");
+        }else {
+            //too low
+            CustomDialog.show("Random guess","You guessed too low.");
+        }
+        guessesRemaining--;
+        lblGuessesRemaining.setText(String.valueOf(guessesRemaining));
     }
 
     @FXML
